62a9979229a343a805519a24e41fa9a9d0ca3aa1
liumeng <378338627@qq.com>
2017-06-14 17:09:38 +0800

    201706141709
    201706141709

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
+import {Hero} from './bean/Hero'
+import {HeroService} from './hero.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes:Hero[];
+  selectedHero:Hero;
+
+  constructor(private heroService:HeroService){}
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
+  };
+  getHeroes():void{
+    //this.heroService.getHeroes().then(heroes=>this.heroes=heroes)
+    this.heroService.getHeroes().then(function(heroes){
+      this.heroes=heroes;
+    })
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from "./bean/Hero";
+export const HEROES:Hero[]=[
+  {id:1,name:'a'},
+  {id:2,name:'b'},
+  {id:3,name:'c'},
+  {id:4,name:'d'},
+  {id:5,name:'e'},
+  {id:6,name:'f'},
+  {id:7,name:'g'},
+  {id:8,name:'h'},
+  {id:9,name:'i'},
+  {id:10,name:'j'}
+];
